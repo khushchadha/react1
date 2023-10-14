5bcd4df852d263d205b512466b3da0168d8a88b7
Kalash Srivastava <kalashsrivastavaoff@gmail.com>
2023-10-14 21:45:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WordCounter from './MyComponent/WordCounter'; // Import the WordCounter component
+
+function App() {
+  return (
+    <div className="App">
+      <WordCounter /> 
+    </div>
+  );
+}
+
+export default App;
